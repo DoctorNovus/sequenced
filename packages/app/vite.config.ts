2923388cd5425b83d6ef08a69311b959940eb56f
jleeson <jleeson2000@gmail.com>
2026-01-01 11:59:09 -0800

    updated vite

---
+++ packages/app/vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import path from "path";
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+    registerType: "prompt",
+    includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+    manifest: {
+        name: "Sequenced: ADHD Management",
+        short_name: "Sequenced",
+        description: "Sequenced ADHD Todo App",
+        icons: [
+            {
+                src: "/assets/icons/icon-256.webp",
+                sizes: "512x512",
+                type: "image/webp",
+                purpose: "favicon"
+            },
+            {
+                src: "/assets/icons/icon-256.webp",
+                sizes: "180x180",
+                type: "image/webp",
+                purpose: "apple touch icon",
+            },
+        ],
+        theme_color: "#307acf",
+        background_color: "#FFFFFF",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait"
+    }
+};
+
+export default defineConfig({
+    server: { port: 3000, allowedHosts: true },
+    resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+    plugins: [react(), VitePWA(manifestForPlugIn)]
+});
